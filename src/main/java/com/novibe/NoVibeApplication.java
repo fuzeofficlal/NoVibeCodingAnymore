b5a25f004b985827baaac78d2b657c44cb4f26fe
fuzeofficial <bochunran@gmail.com>
2026-03-30 14:57:59 +0800

    Add Java Spring Boot project structure

---
+++ src/main/java/com/novibe/NoVibeApplication.java
@@ -0,0 +1,12 @@
+package com.novibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoVibeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoVibeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/novibe/NoVibeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.novibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoVibeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
